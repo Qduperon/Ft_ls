7a316ca9137fee2206a42a1bb90d0f3f01493528
Quentin DUPERON <qduperon@e1r2p9.42.fr>
2016-03-02 15:06:50 +0100

    update

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: qduperon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/26 15:45:48 by qduperon          #+#    #+#             */
-/*   Updated: 2016/02/26 17:34:16 by qduperon         ###   ########.fr       */
+/*   Updated: 2016/03/02 14:56:55 by qduperon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void	ft_error_directory(char *s)
 {
-	ft_putstr("ft_ls: ");
-	ft_putstr(s);
-	ft_putendl(": No such a file or directory");
+	ft_putstr_fd("ft_ls: ", 2);
+	ft_putstr_fd(s, 2);
+	ft_putendl_fd(": No such a file or directory", 2);
 	exit(1);
 }
 
 void	ft_error_flags(char c)
 {
-	ft_putstr("ft_ls: illegal option -- ");
-	ft_putchar(c);
-	ft_putchar('\n');
-	ft_putendl("usage: ft_ls [-Ralrt] [file ...]");
+	ft_putstr_fd("ft_ls: illegal option -- ", 2);
+	ft_putchar_fd(c, 2);
+	ft_putchar_fd('\n', 2);
+	ft_putendl_fd("usage: ft_ls [-Ralrt] [file ...]", 2);
 	exit(1);
 }
